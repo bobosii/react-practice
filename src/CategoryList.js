b430ac3782ce45fa92d2063e267295b5917263b0
Emirhan DAY <emirhanday@Emirhan-Air.home>
2023-09-16 23:37:02 +0300

    changeCategory function added

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -2,23 +2,40 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-    state = {
-        categories: [
-          { categoryId: 1, categoryName: "Beverages" },
-          { categoryId: 2, categoryName: "Condiments" },
-        ],
-      };
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiments" },
+    ],
+    currentCategory: "",
+  };
+  changeCategory = (category)=>{
+    this.setState({ currentCategory: category.categoryName })
+  }
+
   render() {
     return (
       <div>
         <h2>{this.props.info.title}</h2>
         <ListGroup>
-          {this.state.categories.map((category) => ( // her bir kategori için bir li oluştur demek.
-            <ListGroupItem key = {category.categoryId} >{category.categoryName}</ListGroupItem>
-            // Key vermemizin sebebi her bir elemanı döndürürken onu eşsiz yapan id vermemizi istiyor
-            // Çünkü performans odaklı. key vermezsende çalışır fakat konsolda error görürsün.
-          ))}
+          {this.state.categories.map(
+            (
+              category // her bir kategori için bir li oluştur demek.
+            ) => (
+              <ListGroupItem
+                onClick={() => this.changeCategory(category)
+                  // onclick eventi bir fonksiyon gönderdiği için ()=> şeklinde yazmalısın
+                }
+                key={category.categoryId}
+              >
+                {category.categoryName}
+              </ListGroupItem>
+              // Key vermemizin sebebi her bir elemanı döndürürken onu eşsiz yapan id vermemizi istiyor
+              // Çünkü performans odaklı. key vermezsende çalışır fakat konsolda error görürsün.
+            )
+          )}
         </ListGroup>
+        <h3>{this.state.currentCategory}</h3>
       </div>
     );
   }
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 
 export default class ProductList extends Component {
-    constructor(props){
-        super(props)
-        state:{}
-    }
+    
   render() {
     return (
       <div>
