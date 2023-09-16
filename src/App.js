8fd3d27bee1744ddb1b56b3e230363a1d72a8203
Emirhan DAY <emirhanday@Emirhan-Air.home>
2023-09-16 23:26:43 +0300

    categoryList updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
-
+import Navi from "./Navi";
+import { Container, Row , Col} from "reactstrap";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
 
 function App() {
-  return <div>
-    <h2>Hello React</h2>
-  </div>;
+  let productInfo = {title:"ProductList"};
+  let categoryInfo = {title:"CategoryList"}
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo}/>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/CategoryList.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+export default class CategoryList extends Component {
+    state = {
+        categories: [
+          { categoryId: 1, categoryName: "Beverages" },
+          { categoryId: 2, categoryName: "Condiments" },
+        ],
+      };
+  render() {
+    return (
+      <div>
+        <h2>{this.props.info.title}</h2>
+        <ListGroup>
+          {this.state.categories.map((category) => ( // her bir kategori için bir li oluştur demek.
+            <ListGroupItem key = {category.categoryId} >{category.categoryName}</ListGroupItem>
+            // Key vermemizin sebebi her bir elemanı döndürürken onu eşsiz yapan id vermemizi istiyor
+            // Çünkü performans odaklı. key vermezsende çalışır fakat konsolda error görürsün.
+          ))}
+        </ListGroup>
+      </div>
+    );
+  }
+}
